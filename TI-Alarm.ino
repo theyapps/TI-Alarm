964285b8c9e72e601dddbb5b8d4fc6acbfdb7717
Ryan C. Boykin <ryan@theyapps.com>
2015-10-27 22:19:04 -0500

    Implemented a web connected control panel

--- TI-Alarm.ino
+++ TI-Alarm.ino
@@ -1,3 +1,9 @@
+#ifndef __CC3200R1M1RGC__
+// Do not include SPI for CC3200 LaunchPad
+#include <SPI.h>
+#endif
+#include <WiFi.h>
+
 // Sonic wave control
 #define ECHO 7 // Echo Pin
 #define TRIG 18 // Trigger Pin
@@ -6,20 +12,31 @@
 
 #define TOLERANCE 0.5 // A number that represents how much deviation we allow.
 
-int i;
+int i, j;
 int baseline = 0;
 int sonicSum;
 int armed = false;
 int alarm = false;
 int sonicRead = 0;
 
+// Network name
+char ssid[] = "NSA-Probe23";
+// Network password
+char password[] = "TG1672G5D1CE2";
+// your network key Index number (needed only for WEP)
+int keyIndex = 0;
+
+WiFiServer server(80);
+
 /**
 *  Setup Function
 */ 
 void setup(){
+  Serial.begin(115200);      // initialize serial communication
+  
+  /* Initialize pins */
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
-  Serial.begin(115200);      // initialize serial communication
   
   pinMode(GREEN_LED, OUTPUT);      // set the LED pin mode
   pinMode(YELLOW_LED, OUTPUT);      // set the LED pin mode
@@ -31,6 +48,35 @@ void setup(){
   pinMode(PUSH2, INPUT_PULLUP); 
   pinMode(PUSH2, INPUT);
 
+  /* Initialize WIFI */
+  // print the network name (SSID);
+  Serial.print("Attempting to connect to Network named: ");
+  Serial.println(ssid); 
+  // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+  WiFi.begin(ssid, password);
+  while ( WiFi.status() != WL_CONNECTED) {
+    // print dots while we wait to connect
+    Serial.print(".");
+    delay(300);
+  }
+  
+  Serial.println("\nYou're connected to the network");
+  Serial.println("Waiting for an ip address");
+  
+  while (WiFi.localIP() == INADDR_NONE) {
+    // print dots while we wait for an ip addresss
+    Serial.print(".");
+    delay(300);
+  }
+
+  // you're connected now, so print out the status  
+  printWifiStatus();
+  
+  Serial.println("Starting webserver on port 80");
+  server.begin();                           // start the web server on port 80
+  Serial.println("Webserver started!");
+
+  /* Initialize LED states to OFF */
   setLEDs("000");
 }
 
@@ -38,6 +84,8 @@ void setup(){
 *  Main Loop
 */
 void loop(){
+  runServer();
+  
   if(digitalRead(PUSH1) == HIGH){
     setArmed(true);  
   }
@@ -105,7 +153,7 @@ void setArmed(int val){
     
     Serial.println("System Armed!");
   }
-  else if(armed) {
+  else if(!val && armed) {
     baseline = 0;
     armed = false;
     alarm = false;
@@ -162,5 +210,131 @@ int setLEDs(char* flags){
   }
   if(flags[2] == '0'){
     digitalWrite(RED_LED, LOW);
+  }
+}
+
+/**
+*  a way to check if one array ends with another array
+*/
+boolean endsWith(char* inString, char* compString) {
+  int compLength = strlen(compString);
+  int strLength = strlen(inString);
+  
+  //compare the last "compLength" values of the inString
+  int i;
+  for (i = 0; i < compLength; i++) {
+    char a = inString[(strLength - 1) - i];
+    char b = compString[(compLength - 1) - i];
+    if (a != b) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+*  a way to check if one array ends with another array
+*/
+boolean beginsWith(char* inString, char* compString) {
+  int compLength = strlen(compString);
+  int strLength = strlen(inString);
+  
+  //compare the last "compLength" values of the inString
+  int i;
+  for (i = 0; i < compLength; i++) {
+    char a = inString[i];
+    char b = compString[i];
+    if (a != b) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+*  Print details about the wifi connection.
+*/
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("Network Name: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void runServer(){
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    char buffer[150] = {0};                 // make a buffer to hold incoming data
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (strlen(buffer) == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.println("<html><head><title>Group 3 Alarm System</title></head><body align=center>");
+            client.println("<h1 align=center><font color=\"red\">Group 3 Alarm System</font></h1>");
+            client.print("Alarm is: ");
+            if(armed){
+              client.print("Armed");
+              if(alarm){
+                client.println(" and triggered.");
+              }
+              client.println("<br /><br />");
+            }
+            else {
+              client.println("Disarmed <br /><br />");
+            }
+            client.print("<button onclick=\"location.href='/ARM'\">Arm</button> ");
+            client.println("<button onclick=\"location.href='/DISARM'\">Disarm</button><br />");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          }
+          else {      // if you got a newline, then clear the buffer:
+            memset(buffer, 0, 150);
+            j = 0;
+          }
+        }
+        else if (c != '\r') {    // if you got anything else but a carriage return character,
+          buffer[j++] = c;      // add it to the end of the currentLine
+        }
+
+        if(endsWith(buffer, "GET /ARM")) {
+          Serial.println("\nWeb client arming system...");
+          setArmed(true);
+        }
+        if(endsWith(buffer, "GET /DISARM")) {
+          Serial.println("\nWeb client disarming system...");
+          setArmed(false);
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
   }
 }
